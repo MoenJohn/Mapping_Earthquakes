fe3b9880be878f4f6026efb856a85b6c28cd7f4c
[John Moen] <[gionnym4@gmail.com]>
2022-06-16 18:16:41 -0500

    Added line to map

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,46 +1,31 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level (Over the USA).
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // Create the tile layer that will be the background of map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: "streets-v11",
+    id: "satellite-streets-v11",
     accessToken: API_KEY
 });
 
 // Add 'graymap' tile layer to map.
 streets.addTo(map);
 
-// Get data from cities.js
-let cityData = cities;
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+    color: "yellow"
+ }).addTo(map);
   
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
-
-
-
-
-
-// Create map obect witha  center and v
-
-
-//  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-// //  Add a circle to the map for Los Angeles, California.
-// L.circleMarker([34.0522, -118.2437], {
-//     radius: 300,
-//     color:"black",
-//     fillColor: '#ffffa1'
-//  }).addTo(map);
+  
